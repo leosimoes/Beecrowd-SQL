32b8bea74c6fe0c885ea26ec9e06d7a11675becc
Leonardo <leosimoes.engcomp@gmail.com>
2023-03-15 10:41:15 -0300

    Beecrowd-SQL-2602

---
+++ Beecrowd-SQL-2602-Basic-Select/Beecrowd-SQL-2602-Basic-Select.sql
@@ -0,0 +1,10 @@
+/**
+ * Author: Leonardo Simões
+ * Date: 03/13/2023
+ * Beecrowd - SQL - 2602 - Basic Select
+ * Language: PostgreSQL
+ */
+
+SELECT c.name
+FROM customers c
+WHERE c.state LIKE 'RS';
